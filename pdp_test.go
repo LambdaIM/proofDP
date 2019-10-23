f81210d71c38e26d3b06e4a9f1106746723ef5c0
tjan <earltjan@foxmail.com>
2019-10-23 22:00:02 +0800

    Add a convenient test case for application debug. Add go.mod for project integration.

--- pdp_test.go
+++ pdp_test.go
@@ -19,19 +19,25 @@ package proofDP
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"math/rand"
 	"os"
+	"strconv"
 	"testing"
 	"time"
 
 	"github.com/LambdaIM/proofDP/math"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/tendermint/tendermint/crypto/secp256k1"
 )
 
 // psuedo-constant
 var sampleFiles = []string{
+	"math/elliptic_test.go",
+	"math/galois_test.go",
+	"math/pairing_test.go",
 	"math/quadratic_test.go",
 }
 
@@ -47,11 +53,11 @@ func TestProofDPScheme(t *testing.T) {
 
 		file, err := os.OpenFile(sampleFilePath, os.O_RDONLY, 0644)
 		assert.NoError(t, err)
-		tag, err := GenTag(sp, pp, []byte(sampleFilePath), file)
+		tag, err := GenTag(sp, pp, []byte(strconv.Itoa(111)), file)
 		assert.NoError(t, err)
 		file.Close()
 
-		chal, err := GenChal([]byte(sampleFilePath))
+		chal, err := GenChal([]byte(strconv.Itoa(111)))
 		assert.NoError(t, err)
 
 		file, err = os.OpenFile(sampleFilePath, os.O_RDONLY, 0644)
@@ -60,7 +66,7 @@ func TestProofDPScheme(t *testing.T) {
 		assert.NoError(t, err)
 		file.Close()
 
-		assert.True(t, VerifyProof(pp, chal, proof))
+		require.True(t, VerifyProof(pp, chal, proof))
 	}
 }
 
@@ -143,4 +149,20 @@ func TestPDPAgainstLoss(t *testing.T) {
 		// make sure the incomplete content fails
 		assert.False(t, VerifyProof(pp, chal, failPrf))
 	}
+}
+
+func TestSampleValue(t *testing.T) {
+	samplePPStr := "lEbWlt9HsuIbNoae9GKi18wjUlDOfGwsyQECXZT44AgH4278Wiz63Ur4WU+eHGJNcJMFgB/EVpau+SOCe1oPsC2LnRYqvpkzZR+gDW7D0F10XQuJZkC0t2F2w4erhI+bNaJCjoCBfjTLfxd3s3PE7ABoezVJxMU97SMya2NNkts=,Ayppy9ovSnxVrTVqmmaanuV5NI3dYCjKxlQ+JF33BRqZ9SCgPBIP/NnfzAQbqR8lpYpAbSL8uZQDNeqXDAXQ2EihIk8hGN4H2sEK2e7OgIeLIJ/bANDg3hOAevZ6Fgs4beOWnGr7hlFERVXy/uWT2bkwqNctizvd1mYoPPK8Yx0=,XkZ2q7tc+MsEOSdjfYyT7A8vV6Jj2u8XkbSfu2ZL/mUP/eLdnjBy0UAG2QFrYlAf8ACiM3n0Yg93NiMaPJx93Yinm7N6KBXNW9Dse8C4ZTo5TUJfjvIp+YU3ESzhrUdiorEQKsTzoZfXp0QZXlBrUwlLqiPXh5sjZ5ZbUmmGUyI="
+	pp, err := ParsePublicParams(samplePPStr)
+	require.NoError(t, err)
+
+	sampleChalStr := "NDI=,BtpUiNsZIyc4vLYBVIx3K3cF7fY="
+	chal, err := ParseChal(sampleChalStr)
+	require.NoError(t, err)
+
+	sampleProofStr := "PF2IOBHOwPrxDxWghgq+55mLbFE=,Q7+FLPCpV50OLadfuYqIm6egFl7Fi7s3mm4cWhDYMyWG8NFiIUW0mctw+CCdIQD8PzF00LE5MdPJsR6C3Ant2B+BDmtIY4ItRDD5zLFpuO1ZBYSxZTSo26OG5UsZeozGqygzvpPQq7KdhVQazNya1IEGhpiC//5YJEXtockQneI=,KBH/X8DQSqfrBHeLgOLyB805rpxhSidxT64LaPklO0Rw+jZQAjZNEQsEDVo9pcFhjkR9yvzq8W03Un7iYGIP83PkjVXduAWzRnV5k2aHVO9lS5oUHE7H1t9icTxmtQG4Q68/JtCeJuOsko0/DVLNj+bxOzQAV5KXDvf5R86uwZk="
+	proof, err := ParseProof(sampleProofStr)
+	require.NoError(t, err)
+
+	fmt.Printf("%t\n", VerifyProof(pp, chal, proof))
 }
